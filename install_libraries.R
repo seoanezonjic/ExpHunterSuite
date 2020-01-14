25741bf02d20d91f0ec882f95a1250a651479f70
Fernando Moreno Jabato <fmjabato@uma.es>
2020-01-14 11:39:26 +0100

    Install script updated by Jose Cordoba

--- install_libraries.R
+++ install_libraries.R
@@ -1,47 +1,29 @@
 #! /usr/bin/env Rscript
 
-
 ######################################################################################################
 ####################### LIBRARY INSTALLING SCRIPT for DEgenes Hunter #################################
 ######################################################################################################
 
-############### 
-###############
-#For using type source("install_libraries.R") in your R console (version R-3.2.3 or higher)
-
-
 ############## Installing latest version of Bioconductor #####################
-## try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-biocLite()
-
+install.packages("BiocManager")
+BiocManager::install(version = "3.10")
+library('BiocManager')
 
 ####################  LIBRARIES FOR DIFFERENTIAL EXPRESSION ANALYSIS SCRIPT (degenes_Hunter.R) ############
-
 print("Installing libraries for differential expression analysis script")
 
 ## PACKAGES IN CRAN
-install.packages(c("ggplot2","VennDiagram","gplots","optparse","stringr","plyr","reshape2","PerformanceAnalytics"))
+install.packages(c("FSA","ggplot2","VennDiagram","gplots","optparse","stringr","plyr","reshape2","PerformanceAnalytics"))
 
 ## PACKAGES IN BIOCONDUCTOR
-## try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-biocLite("limma")
-biocLite("edgeR")
-biocLite("DESeq2")
-biocLite("NOISeq")
-
-
+BiocManager::install(c("limma", "edgeR", "DESeq2", "NOISeq"))
 
 ####################  LIBRARIES required specifically by the FUNCTIONAL ANALYSIS SCRIPT (functional_Hunter.R) ############
-
 print("Installing additional libraries for functional analysis script")
 
 ## PACKAGES IN BIOCONDUCTOR
-## try http:// if https:// URLs are not supported
-## Functional analysis script requires "optparse", "stringr" and "plyr" as well.
+BiocManager::install(c("biomaRt", "topGO", "KEGGREST", "clusterProfiler", "Rgraphviz"))
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("biomaRt")
-biocLite("topGO")
-biocLite("KEGGREST")
+################## Annotation Databases for functional analysis
+## PACKAGES IN BIOCONDUCTOR
+BiocManager::install(c("org.Hs.eg.db")
