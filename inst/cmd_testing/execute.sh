3decc64df35dcc2ac4479b9774b15f758422f257
seoanezonjic <seoanezonjic@hotmail.com>
2020-10-22 13:00:57 +0200

    Moved cmd_testing folder to inst folder

--- inst/cmd_testing/execute.sh
+++ inst/cmd_testing/execute.sh
@@ -3,7 +3,7 @@
 . ~soft_bio_267/initializes/init_degenes_hunter
 
 export DEGHUNTER_MODE=DEVELOPMENT
-export PATH=../inst/scripts:$PATH
+export PATH=../scripts:$PATH
 
 which degenes_Hunter.R
 # grep -o '"\-\-[A-Za-z0-9_]*' options_file | sed 's/"--\([A-Za-z0-9_]*\)/\1=opt$\1,/g'
--- inst/cmd_testing/execute_as_package.sh
+++ inst/cmd_testing/execute_as_package.sh
@@ -4,11 +4,12 @@
 # https://laderast.github.io/2019/02/12/package-building-description-namespace/
 # Rscript -e "usethis::use_package('optparse')" # To add dependencies that are not consigned by roxygen documentation
 
-cd ..
+current=`pwd`
+cd ../../
 Rscript -e "devtools::document()"
 Rscript -e "devtools::install()"
 
-cd cmd_testing
+cd $current
 mkdir install_folder
 Rscript -e "DEgenesHunter::install_DEgenes_hunter('install_folder')"
 install_folder/degenes_Hunter.R -h
