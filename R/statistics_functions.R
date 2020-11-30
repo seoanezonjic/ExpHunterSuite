2a1712fd72a804d9314e9a71c715ab1de24f141a
Fernando Moreno Jabato <fmjabato@uma.es>
2020-11-30 10:13:02 +0100

    Added documentation to statistisc functions

--- R/statistics_functions.R
+++ R/statistics_functions.R
@@ -1,38 +1,43 @@
-#' 
-#' @param df 
-#' @keywords installation
+#' Calculates accuracy from a measures dataframe
+#' @param df dataframe with measures 
+#' @return calculated metric
+#' @keywords metrics
 #' @export
 acc <- function(df){
 	(df$TP + df$TN) / (df$TP + df$TN + df$FP + df$FN)
 }
 
-#' 
-#' @param df 
-#' @keywords installation
+#' Calculates Positive Predictive Value from a measures dataframe
+#' @param df dataframe with measures 
+#' @return calculated metric
+#' @keywords metrics
 #' @export
 ppv <- function(df){
 	df$TP / (df$TP + df$FP)
 }
 
-#' 
-#' @param df 
-#' @keywords installation
+#' Calculates Recall from a measures dataframe
+#' @param df dataframe with measures 
+#' @return calculated metric
+#' @keywords metrics
 #' @export
 recall <- function(df){
 	df$TP / (df$TP + df$FN)
 }
 
-#' 
-#' @param df 
-#' @keywords installation
+#' Calculates specificity from a measures dataframe
+#' @param df dataframe with measures 
+#' @return calculated metric
+#' @keywords metrics
 #' @export
 spc <- function(df){
 	df$TN / (df$TN + df$FP)
 }
 
-#' 
-#' @param df 
-#' @keywords installation
+#' Calculates F measure from a measures dataframe
+#' @param df dataframe with measures 
+#' @return calculated metric
+#' @keywords metrics
 #' @export
 fmeasure <- function(df){
 	2 * (df$Precision * df$Recall) / (df$Precision + df$Recall)
