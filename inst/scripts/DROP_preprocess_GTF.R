b25322eef8fb466e459fbcc78a77c815d5057e27
AEstebanMar <alvaroesteban@uma.es>
2024-05-03 12:38:16 +0200

    Removed output_dir argument from preprocess_gtf call, as it no longer takes a second argument. Writing of files to disk now handled by script. Output_dir now argument of script option parser. If it is NULL, saves to default Hunter DROP directory (recommended)

--- inst/scripts/DROP_preprocess_GTF.R
+++ inst/scripts/DROP_preprocess_GTF.R
@@ -9,6 +9,8 @@ option_list <- list(
   )
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 
+preprocessing_results <- preprocess_gtf(opt$gtf)
+
 if(is.null(opt$output_dir)) {
   ### I want this to point do inst/DROP_data, or root/DROP_data in installed package
   output_dir <- system.file("DROP_data", package= "ExpHunterSuite")
@@ -16,4 +18,12 @@ if(is.null(opt$output_dir)) {
   output_dir <- opt$output_dir
 }
 
-preprocess_gtf(opt$gtf, output_dir)
+gtf_name <- basename(tools::file_path_sans_ext(opt$gtf))
+db_file <- file.path(outdir, paste0(gtf_name, "_txdb.db"))
+cr_file <- file.path(outdir, paste0(gtf_name, "_count_ranges.rds"))
+map_file <- file.path(outdir, paste0(gtf_name, "_gene_name_mapping.tsv"))
+
+AnnotationDbi::saveDb(preprocessing_results$txdb, db_file)
+saveRDS(preprocessing_results$count_ranges, cr_file)
+write.table(preprocessing_results$gene_name_mapping, file = map_file, sep = "\t", quote = FALSE,
+            row.names = FALSE)
