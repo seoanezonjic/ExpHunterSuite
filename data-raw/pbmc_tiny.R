319b2088182fe39753468967b1b90f6b25bc743b
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-01-23 12:33:09 +0100

    Further subset pbmc_tiny, now only contains 10 features

--- data-raw/pbmc_tiny.R
+++ data-raw/pbmc_tiny.R
@@ -2,5 +2,7 @@
 
 library(SeuratObject)
 pbmc_tiny <- pbmc_small[, 1:15]
+gene_list <- c("PPBP", "IGLL5", "VDAC3", "CD1C", "AKR1C3", "PF4", "MYL9", "GNLY", "TREML1", "CA2")
+pbmc_tiny <- downsample_seurat(pbmc_tiny, features = gene_list)
 
 usethis::use_data(pbmc_tiny, overwrite = TRUE)
