7fb3f5dc9d9c6e752673848eff545065c47f0d53
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-02-18 14:45:49 +0100

    Added tMAE functions to DROP functions file. Pending documentation review. Pasted from https://github.com/gagneurlab/tMAE

--- R/DROP_functions.R
+++ R/DROP_functions.R
@@ -730,3 +730,352 @@ get_expressed_genes <- function(ods) {
   rownames(new_df) <- NULL
   return(df[order(df$Rank), ])
 }
+
+
+## tMAE functions. Not in CRAN or bioconductor, and we depend on them, so
+## we're copying them here and crediting the original repo:
+## https://github.com/gagneurlab/tMAE
+
+#' @title Add AF to GRanges
+#' @author [gagneurlab](https://github.com/gagneurlab/tMAE)
+#' @rdname add_gnomAD_AF
+#' @description appending the minor allele frequency to GRanges using gnomAD
+#' @param object either a data.table of allelic counts or a GRanges object
+#' @param genome_assembly one of "hg19", "hs37d5", "hg38", "GRCh38"
+#'                It can also be any full string of a MafDb provided by
+#'                \code{\link[GenomicScores]{availableGScores}}.
+#' @param max_af_cutoff cutoff for a variant to be considered rare. Default 0.001
+#' @param populations The populations to be annotatated.
+#' @param ... Used for backwards compatibility (gene_assembly -> genome_assembly)
+#' @return a data.frame containing original data as well as the minor allele frequencies
+#' @examples
+#' file <- system.file("extdata", "GR_HG00187.Rds", package = "tMAE", mustWork = TRUE)
+#' gr <- readRDS(file)
+#' genome_assembly <- 'MafDb.ExAC.r1.0.hs37d5'
+#' \dontrun{
+#' res <- add_gnomAD_AF(gr, genome_assembly = genome_assembly, populations = "AF")
+#' }
+#'
+#' file <- system.file("extdata", "allelic_counts_HG00187.csv", package = "tMAE", mustWork = TRUE)
+#' maeCounts <- fread(file)                                                                       
+#' maeRes <- DESeq4MAE(maeCounts)                                                                 
+#' \dontrun{
+#' res <- add_gnomAD_AF(maeRes, genome_assembly = genome_assembly, populations = "AF")            
+#' }
+#'                                                                                                
+#' @export
+setGeneric("add_gnomAD_AF",function(
+    object,
+    genome_assembly = 'hg19',
+    max_af_cutoff = .001,
+    populations = c('AF', 'AF_afr', 'AF_amr', 'AF_eas', 'AF_nfe', 'AF_popmax'),
+    ...) standardGeneric("add_gnomAD_AF"))
+
+
+# Helper method for scoring a GRanges object. Returns GRanges object
+score_data <- function(gr, 
+    genome_assembly = 'hg19',
+    max_af_cutoff = .001,
+    populations = c('AF', 'AF_afr', 'AF_amr', 'AF_eas', 'AF_nfe', 'AF_popmax'),
+    ...){
+  if("gene_assembly" %in% names(list(...))){
+    warning("'gene_assembly' is deprecated. Please use 'genome_assembly' instead.")
+    genome_assembly <- list(...)[['gene_assembly']]
+  }
+  
+  if(genome_assembly %in% BiocManager::available("MafDb")){
+    mafdb <- .get_mafdb(genome_assembly)
+  } else {
+    mafdb <- .get_mafdb(switch(genome_assembly, 
+      hg19   = "MafDb.gnomAD.r2.1.hs37d5",
+      hs37d5 = "MafDb.gnomAD.r2.1.hs37d5",
+      hg38   = "MafDb.gnomAD.r2.1.GRCh38",
+      GRCh38 = "MafDb.gnomAD.r2.1.GRCh38",
+      stop("Please provide a supported genome assembly version. Not: ", genome_assembly)
+    ))
+  }
+    
+  if(!all(populations %in% populations(mafdb))){
+    stop("Please provide only populations provided by gnomAD!")
+  }
+  
+  # Add score of all, African, American, East Asian and Non-Finnish European
+  gr_scores <- gscores(mafdb, gr, pop = populations)
+  return(gr_scores)
+}
+
+
+# Helper method for installilng the named MafDb package
+.get_mafdb <- function(pkg_name){
+  if(!requireNamespace(pkg_name, quietly=TRUE)){
+    warning("The given MafDb is not installed: '", pkg_name, "'. We will do it now!")
+    if(!requireNamespace("BiocManager", quietly=TRUE)){
+      install.packages("BiocManager")
+    }
+    BiocManager::install(pkg_name, ask=FALSE)
+  }
+  
+  mafdb <- getFromNamespace(pkg_name, pkg_name)
+  mafdb
+}
+
+#' @rdname add_gnomAD_AF
+setMethod("add_gnomAD_AF", signature = "data.table", 
+function(
+    object, 
+    genome_assembly = 'hg19',
+    max_af_cutoff = .001,
+    populations = c('AF', 'AF_afr', 'AF_amr', 'AF_eas', 'AF_nfe', 'AF_popmax'),
+    ... ){
+  # create GRanges from a MAE count table
+  gr <- GRanges(seqnames = object$contig,
+                ranges = IRanges(start=object$position, width=1), 
+                strand = '*')
+  # score the gr
+  gr_scores <- score_data(gr,genome_assembly=genome_assembly,populations = populations, max_af_cutoff = max_af_cutoff,...)
+  
+  # merge the population scores with the original object
+  scores <- data.table::as.data.table(gr_scores)[,..populations]
+  res <- cbind(object, scores) |> data.table::as.data.table()
+  
+  # Compute the MAX_AF based on all provided population columns
+  # return -1 if only NAs are present (to avoid a warning)
+  res$MAX_AF <- apply(res[, ..populations], 1, 
+                      FUN=function(x){ max(x, -1, na.rm=TRUE) })
+  
+  # Replace Inf/-1 with NA
+  res[is.infinite(MAX_AF) | MAX_AF == -1, MAX_AF := NA]
+  res[, rare := (MAX_AF <= max_af_cutoff | is.na(MAX_AF))]
+  return (res)
+})
+
+#' @rdname add_gnomAD_AF
+setMethod("add_gnomAD_AF", signature = "GRanges",
+function(
+    object, 
+    genome_assembly = 'hg19',
+    max_af_cutoff = .001,
+    populations = c('AF', 'AF_afr', 'AF_amr', 'AF_eas', 'AF_nfe', 'AF_popmax'),
+    ...) {
+
+  # score the original GRanges object 
+  scores <- score_data(object,
+    genome_assembly = genome_assembly,
+    max_af_cutoff = max_af_cutoff,
+    populations = populations,
+    ...)
+  return(scores)
+})
+
+#' plotMA4MAE
+#'
+#' @description Creates an MA plot, ie, Fold Change (ALT/REF) vs Coverage colored 
+#'    by significance (from p adjusted and allelic ratio) or significance and 
+#'    minor allele frequency (if rare_column is provided).
+#' @author Vicente Yepez
+#' @param data A data.frame containing the results table from \code{DESeq4MAE} function
+#' @param title The plot's title
+#' @param padjCutoff The significance level
+#' @param allelicRatioCutoff The minimum allelic ratio ALT/(ALT+REF) to be 
+#'    considered signficant
+#' @param rare_column The name of the column that indicates if a variant is rare or not.
+#'    Default is \code{null} which means it won't be plotted.
+#' @return A ggplot object containing the MA plot.
+#' @export
+#' @examples
+#' file <- system.file("extdata", "allelic_counts_HG00187.csv",
+#'     package = "tMAE", mustWork = TRUE)
+#' maeCounts <- fread(file)
+#' res <- DESeq4MAE(maeCounts)
+#' plotMA4MAE(res)
+
+plotMA4MAE <- function(data, title = NULL, padjCutoff = 0.05, 
+                   allelicRatioCutoff = .8, rare_column = NULL){
+  stopifnot(c('altCount', 'refCount', 'altRatio', 'padj') %in% colnames(data))
+  
+  data <- data.table::as.data.table(data)
+  data[, FC := (altCount + 1)/(refCount + 1)]
+  data[, Significant := padj <= padjCutoff & 
+         (altRatio > allelicRatioCutoff | altRatio < (1-allelicRatioCutoff))]
+  
+  # Make the sketch of the plot
+  g <- ggplot2::ggplot(data, ggplot2::aes(totalCount, FC)) + 
+    ggplot2::theme_bw(base_size = 14) + ggplot2::scale_y_log10() + ggplot2::scale_x_log10() +
+    ggplot2::labs(x = 'RNA Coverage per heterozygous SNV', 
+         y = 'Fold change of allelic counts\n(ALT+1)/(REF+1)', title = title)
+  
+  # If rare_column provided, combine it with Significant column and plot
+  if(!is.null(rare_column)){
+    stopifnot(rare_column %in% colnames(data))
+    data[, class := 'NS']
+    data[Significant == T & get(rare_column) == T, class := 'Significant\n& Rare']
+    data[Significant == T & get(rare_column) == F, class := 'Significant']
+    g <- g + ggplot2::geom_point(ggplot2::aes(col = class), size = .9) +
+      ggplot2::scale_color_manual(values = c('gray61', 'chocolate1', 'firebrick')) +
+      ggplot2::theme(legend.title = ggplot2::element_blank())
+  } else{
+    g <- g + ggplot2::geom_point(ggplot2::aes(col = Significant), size = .9) +
+      ggplot2::scale_color_manual(values = c('gray61', 'firebrick'))
+  }
+  
+  return(g)
+}
+
+
+#' plotAllelicCounts
+#'
+#' @description Creates an allelic counts plot, ie, counts of the alternative
+#'    vs counts of the reference on the log10 scale. If significant and minor
+#'    allele frequency columns present, dots will be highlighted accordingly.
+#' @author Vicente Yepez
+#' @param data A data.frame containing the counts or results table from \code{DESeq4MAE} function
+#' @param title The plot's title
+#' @param padjCutoff The significance level
+#' @param allelicRatioCutoff The minimum allelic ratio ALT/(ALT+REF) to be 
+#'    considered signficant
+#' @param rare_column The name of the column that indicates if a variant is rare or not.
+#'    Default is \code{null} which means it won't be plotted.
+#' @return A ggplot object containing the MA plot.
+#' @export
+#' @examples
+#' file <- system.file("extdata", "allelic_counts_HG00187.csv", 
+#'     package = "tMAE", mustWork = TRUE)
+#' maeCounts <- fread(file)
+#' res <- DESeq4MAE(maeCounts)
+#' plotAllelicCounts(res)
+#' 
+plotAllelicCounts <- function(data, title = NULL, padjCutoff = 0.05, 
+                       allelicRatioCutoff = .8, rare_column = NULL){
+  stopifnot(c('altCount', 'refCount') %in% colnames(data))
+  
+  data <- data.table::as.data.table(data)
+  
+  # Make the sketch of the plot
+  g <- ggplot2::ggplot(data, ggplot2::aes(refCount, altCount)) + 
+    ggplot2::theme_bw(base_size = 14) + ggplot2::scale_y_log10() + ggplot2::scale_x_log10() +
+    ggplot2::labs(x = 'REF count + 1', y = 'ALT count + 1', title = title) + 
+    ggplot2::geom_abline(slope = 1, intercept = 0)
+  
+  # If significance column is provided, add it to the plot
+  if(!is.null(data$padj)){
+    data[, Significant := padj <= padjCutoff & 
+           (altRatio > allelicRatioCutoff | altRatio < (1-allelicRatioCutoff))]
+  }
+  
+  # If rare_column provided, combine it with Significant column and plot
+  if(!is.null(rare_column)){
+    stopifnot(rare_column %in% colnames(data))
+    data[, class := 'NS']
+    data[Significant == T & get(rare_column) == T, class := 'Significant\n& Rare']
+    data[Significant == T & get(rare_column) == F, class := 'Significant']
+    g <- g + ggplot2::geom_point(ggplot2::aes(col = class), size = .9) +
+      ggplot2::scale_color_manual(values = c('gray61', 'chocolate1', 'firebrick')) +
+      ggplot2::theme(legend.title = ggplot2::element_blank())
+  } else{
+    g <- g + ggplot2::geom_point(ggplot2::aes(col = Significant), size = .9) +
+      ggplot2::scale_color_manual(values = c('gray61', 'firebrick')) 
+  }
+  return(g)
+}
+
+# Converts a GRanges object to a Data Table
+allelic_granges_to_dt <- function(data){
+    # take only heterozygous mutation and with enough coverage
+    data$GT <- as.character(data$GT)
+    goodGT <- grepl('0[|/]1|1[|/]0', data$GT, perl=TRUE)
+    # goodGT <- grepl('0[|/]1|1[|/]0', data$GT[,1], perl=TRUE)
+    
+    data$GQ <- as.integer(data$GQ)
+    goodCov <- data$coverage > 0
+    data <- data[goodGT & goodCov]
+    
+    data$nucl_piles = NULL
+    data$qual_piles = NULL
+    
+    # get alt and ref counts
+    data$chr <- GenomeInfoDb::seqnames(data)
+    data$alt_cov <- as.integer(floor(data$coverage * data$alt_allele_freq))
+    data$ref_cov <- as.integer(data$coverage - data$alt_cov)
+    data$pos <- start(data)
+    data$ALT <- as.character(unlist(data$ALT))
+    dt <- data.table::as.data.table(data)
+    dt[, c("start", "end", "width", "strand", "QUAL", "FILTER", "GQ", "A", "C", "G", "T", "seqnames") := NULL]
+    return(dt)
+}
+
+
+# Performs the Negative Binomial Wald Test
+deseq_for_allele_specific_expression <- function(data, minCoverage=10,
+                                                     disp=0.05, independentFiltering=FALSE){
+    # Obtain a data.table from imput
+    if(any(class(data) == 'data.frame')){
+        dt <- data.table::as.data.table(data)
+        dt[, c('lowMAPQDepth', 'lowBaseQDepth', 'rawDepth', 'otherBases', 'improperPairs') := NULL]
+    } else if(class(data) == 'GRanges'){
+        dt <- allelic_granges_to_dt(data)
+    }
+    
+    dt <- dt[totalCount >= minCoverage]
+    
+    # create deseq object
+    dds <- DESeq2::DESeqDataSetFromMatrix(
+        as.matrix(dt[, .(altCount, refCount)]),
+        S4Vectors::DataFrame(condition=factor(c("altAllele", "refAllele"))),
+        design = ~ condition
+    )
+    
+    if(!is.null(data$hgncid))
+        rownames(dds) <- data[, paste0(hgncid, "_", pos)]  # Doesn't always have the gene name
+    
+    # mcols(rowRanges(dds)) <- dt
+    
+    # estimate the size factors and pseudo dispersion
+    dds <- BiocGenerics::estimateSizeFactors(dds)
+    # dds <- estimateDispersions(dds) # impossible to determine now with only 1 sample and 2 conditions
+    
+    # set dispersion by hand
+    DESeq2::dispersions(dds) <- disp
+    
+    # run wald test
+    dds <- DESeq2::nbinomWaldTest(dds)
+    res <- DESeq2::results(dds, contrast = c("condition", "altAllele", "refAllele"), 
+                   independentFiltering = independentFiltering)
+    return(list(dt = dt, res = res))
+}
+
+
+# Get Allele Specific deseq results
+get_allele_specific_deseq_results <- function(dds_res){
+    # get needed info
+    dt <- dds_res$dt
+    # add pvalue and padj
+    dt[, c("pvalue","padj", "log2FC") := list(dds_res$res$pvalue, dds_res$res$padj, dds_res$res$log2FoldChange)]
+    # add ratio the alternative allele
+    dt[, altRatio := altCount / (altCount + refCount)]
+    return(dt)
+}
+
+#' Run deseq test for MAE
+#'
+#' @description Uses a negative binomial test to determine if a variant is mono-allelically expressed.
+#' @author Vicente Yepez, Christian Mertes
+#' @param data A data.frame containing allelic counts.
+#' @param minCoverage minimum total allelic count. Default is 10.
+#' @param disp Gene dispersion for the NB test. Default is 0.05.
+#' @param independentFiltering Parameter that affects the multiple testing. Default is FALSE.
+#' @return Mono-allelic results table containing original counts plus p-value, p-adjusted and freqALT columns.
+#' @export
+#' @examples
+#' file <- system.file("extdata", "allelic_counts_HG00187.csv", package = "tMAE", mustWork = TRUE)
+#' maeCounts <- fread(file)
+#' DESeq4MAE(maeCounts)
+DESeq4MAE <- function(data, minCoverage = 10, disp = .05, independentFiltering = FALSE){
+    
+    pt <- deseq_for_allele_specific_expression(data, minCoverage=minCoverage, disp=disp, 
+                                                   independentFiltering=independentFiltering)
+    
+    res <- get_allele_specific_deseq_results(pt)
+    
+    return(res)
+}
+
