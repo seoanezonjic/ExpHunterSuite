b782bb70d63fe2657d1b0382516830a4811cb8e8
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-04-02 16:41:47 +0200

    Removed unused option arguments, and changed DEG_target argument for targets_folder. Removed DEG_target generation, as it is now handled by external script, now this one creates a list of targets by reading target files. main_sc_Hunter now called in parallel for each target defined.

--- inst/scripts/sc_Hunter.R
+++ inst/scripts/sc_Hunter.R
@@ -25,8 +25,8 @@ if( Sys.getenv('DEGHUNTER_MODE') == 'DEVELOPMENT' ){
 option_list <- list(
   optparse::make_option(c("-n", "--name"), type = "character", default = NULL,
             help = "Experiment name."),
-  optparse::make_option("--DEG_target", type = "character", default = "",
-            help = "A string describing DEG analyses to perform."),
+  optparse::make_option("--targets_folder", type = "character", default = "",
+            help = "Directory containing target files"),
   optparse::make_option("--p_val_cutoff", type = "numeric", default = "5e-3",
             help = "Adjusted p-value cutoff."),
   optparse::make_option("--min_avg_log2FC", type = "numeric", default = 1e-3,
@@ -38,58 +38,43 @@ option_list <- list(
   optparse::make_option(c("-o", "--output"), type = "character", default = NULL,
             help = "Output folder."),
   optparse::make_option("--input", type = "character", default = NULL,
-            help = "Directory containing counts"),
-  optparse::make_option("--subset_by", type = "character", default = "",
-            help = "Comma-separated list of conditions by which to perform integration
-                    analysis. If empty, all conditions will be analysed."),
+            help = "Directory containing processed single-cell counts and metadata."),
   optparse::make_option("--target_genes", type = "character", default = "",
             help = "Path to target genes table, or comma-separated list of target genes."),
-  optparse::make_option("--cpu", type = "double", default = 1,
-            help = "Provided CPUs."),
   optparse::make_option("--verbose", type = "logical", default = FALSE, action = "store_true",
-            help = "Verbosity of base Seurat and harmony function calls.")
+            help = "Verbosity of base Seurat and harmony function calls."),
+  optparse::make_option("--cpu", type = "integer", default = 1,
+            help = "Provided CPUs.")
 )
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
 
-if(opt$DEG_target == "") {
-  message("DEG analysis disabled.")
-  DEG_target <- NULL
+if(opt$targets_folder == "" | !file.exists(opt$targets_folder)) {
+  stop(paste0("Provided targets directory does not exist. Was ", opt$targets_folder))
 } else {
-  DEG_target <- unlist(strsplit(opt$DEG_target, ";"))
-  DEG_target <- unlist(lapply(DEG_target, strsplit, split = ":"), recursive = FALSE)
-  DEG_target <- lapply(DEG_target, function(vector) {
-    if(length(vector) < 2) {
-      res <- c(vector, "all")
-    } else {
-      res <- vector
-    }
-    return(res)
-  })
-  DEG_names <- unlist(lapply(DEG_target, `[[`, 1))
-  DEG_names <- strsplit(DEG_names, ">")
-  DEG_columns <- unlist(lapply(DEG_names, `[[`, 2))
-  DEG_names <- unlist(lapply(DEG_names, `[[`, 1))
-  DEG_values <- unlist(lapply(DEG_target, `[[`, 2))
-  DEG_target <- data.frame(column = tolower(DEG_columns), values = DEG_values)
-  rownames(DEG_target) <- DEG_names
+  target_files <- dir(opt$targets_folder, full.names = TRUE)
+  DEG_targets <- vector(mode = "list", length = length(target_files))
+  names(DEG_targets) <- target_files
+  for(target_file in target_files) {
+    DEG_targets[[target_file]] <- read.table(target_file, header = TRUE)
+  }
+  names(DEG_targets) <- tools::file_path_sans_ext(basename(target_files))
+}
+
+if(opt$cpu > 1) {
+  BiocParallel::register(BiocParallel::MulticoreParam(opt$cpu))
+  BPPARAM <- BiocParallel::MulticoreParam(opt$cpu)
+} else {
+  BPPARAM <- BiocParallel::SerialParam()
 }
 
 if(opt$target_genes == ""){
-  warning("No target genes provided")
+  message("No target genes provided")
   target_genes <- NULL
 } else if(file.exists(opt$target_genes)) {
   target_genes <- read_and_format_targets(opt$target_genes)
 } else {
-  target_genes <- list(Custom = strsplit(opt$target_genes, split = ";")[[1]])
-}
-
-subset_by <- NULL
-if(opt$subset_by == "") {
-  message("No conditions specified for analysis. Data will not be subset.")
-} else {
-  subset_by <- tolower(unlist(strsplit(opt$subset_by, ",")))
-  message(paste0("Selected ", length(subset_by), " subset condition(s): ", paste0(subset_by, collapse = ", ")))    
+  target_genes <- strsplit(opt$target_genes, split = ";")[[1]]
 }
 
 message("Reconstructing Seurat object from directory ", opt$input)
@@ -100,7 +85,10 @@ seu_meta <- read.table(file.path(opt$input, "meta.tsv"), sep = "\t", header = TR
 rownames(seu_meta) <- colnames(seu)
 seu <- Seurat::AddMetaData(seu, seu_meta, row.names("Cell_ID"))
 seu$RNA$data <- seu$RNA$counts
-DEG_results <- main_sc_Hunter(seu = seu, DEG_target = DEG_target, p_val_cutoff = opt$p_val_cutoff,
-                              min_avg_log2FC = opt$min_avg_log2FC, min_cell_proportion = opt$min_cell_proportion,
-                              query = unlist(target_genes), subset_by = subset_by, output_path = opt$output,
-                              min_counts = opt$min_counts, verbose = opt$verbose)
+save.image('testing.RData')
+stop('test')
+DEG_results <- BiocParallel::bplapply(X = DEG_targets, FUN = main_sc_Hunter, BPPARAM = BPPARAM, seu = seu,
+                                      p_val_cutoff = opt$p_val_cutoff, min_avg_log2FC = opt$min_avg_log2FC,
+                                      min_cell_proportion = opt$min_cell_proportion, query = target_genes,
+                                      output_path = opt$output, min_counts = opt$min_counts, verbose = opt$verbose)
+names(DEG_results) <- unlist(strsplit(names(DEG_targets), "_target"))
