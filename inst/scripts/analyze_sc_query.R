6c39f31001863586eab20f1a1230dffd85ae5161
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-04-03 16:39:12 +0200

    Script now takes embeddings, verbose and extra_columns arguments, and they process them accordingly. Now calls report writing function (templates still need fixing).

--- inst/scripts/analyze_sc_query.R
+++ inst/scripts/analyze_sc_query.R
@@ -27,6 +27,8 @@ option_list <- list(
             help = "Experiment name."),
   optparse::make_option("--input", type = "character", default = NULL,
             help = "Directory containing processed single-cell counts and metadata."),
+  optparse::make_option("--embeddings", type = "character", default = NULL,
+            help = "Directory containing embedding information."),
   optparse::make_option("--target_genes", type = "character", default = "",
             help = "Path to target genes table, or comma-separated list of target genes."),
   optparse::make_option("--sigfig", type = "integer", default = 3,
@@ -34,7 +36,11 @@ option_list <- list(
   optparse::make_option(c("-o", "--output"), type = "character", default = NULL,
             help = "Output folder."),
   optparse::make_option("--cpu", type = "double", default = 1,
-            help = "Provided CPUs.")
+            help = "Provided CPUs."),
+  optparse::make_option("--verbose", type = "logical", default = FALSE, action = "store_true",
+            help = "Verbosity of base Seurat and harmony function calls."),
+  optparse::make_option("--extra_columns", type = "character", default = "",
+            help = "Comma-separated list of extra conditions to represent in certain plots.")
 )
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
@@ -48,10 +54,27 @@ if(opt$target_genes == ""){
   target_genes <- strsplit(opt$target_genes, split = ";")[[1]]
 }
 
+if(opt$extra_columns == "") {
+  extra_columns <- NULL
+} else {
+  extra_columns <- tolower(unlist(strsplit(opt$extra_columns, ",")))
+}
+
 seu <- Seurat::CreateSeuratObject(counts = Seurat::Read10X(opt$input, gene.column = 1),
                                   project = opt$name, min.cells = 1, min.features = 1)
 seu_meta <- read.table(file.path(opt$input, "meta.tsv"), sep = "\t", header = TRUE)
 rownames(seu_meta) <- colnames(seu)
 seu <- Seurat::AddMetaData(seu, seu_meta, row.names("Cell_ID"))
 seu$RNA$data <- seu$RNA$counts
+embeddings <- read.table(file.path(opt$embeddings, "cell_embeddings.txt"), header = TRUE)
+seu$UMAP_full <- Seurat::CreateDimReducObject(embeddings = as.matrix(embeddings), key = 'UMAPfull_', assay = 'RNA')
 query_data <- main_analyze_sc_query(seu = seu, query = target_genes, sigfig = opt$sigfig)
+
+query_results <- list(seu = seu, query_data = query_data)
+message("--------------------------------------------")
+message("-------Writing query analysis report--------")
+message("--------------------------------------------")
+
+write_sc_report(final_results = query_results, template_folder = template_folder, output = file.path(opt$output, "report"),
+                query = unlist(target_genes), extra_columns = extra_columns,
+                template = "sc_analysis.txt", out_name = "query_report.html", opt = opt)
