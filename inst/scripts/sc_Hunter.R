9d250437946df178ccae86de26180bfdd71a191a
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-05-02 16:00:44 +0200

    Updated out_name argument in write_sc_report call, is now out_suffix

--- inst/scripts/sc_Hunter.R
+++ inst/scripts/sc_Hunter.R
@@ -92,5 +92,5 @@ for(target_name in names(DEG_targets)) {
   DEG_results <- list(seu = seu, DEG_list = DEG_list[[DEG_name]], opt = opt, target = DEG_targets[[target_name]], target_name = DEG_name)
   write_sc_report(final_results = DEG_results, query = target_genes, opt = opt,
                   template_folder = template_folder, output = file.path(opt$output, "report"), 
-                  template = "sc_DEGs.txt", out_name = paste0(DEG_name, "_DEG_report.html"))
+                  template = "sc_DEGs.txt", out_suffix = paste0(DEG_name, "_DEG_report.html"))
 }
