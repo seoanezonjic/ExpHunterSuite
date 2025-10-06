cccd471c96f272a33c485f38ab413d0ef9359f7b
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-10-06 14:54:18 +0200

    Updated script to obtain pbmc_tiny object, selected cells are no longer random. Also updated it to a v5.2.0 Seurat object. Also updated rda file.

--- data-raw/pbmc_tiny.R
+++ data-raw/pbmc_tiny.R
@@ -3,6 +3,15 @@
 library(SeuratObject)
 pbmc_tiny <- pbmc_small[, 1:15]
 gene_list <- c("PPBP", "IGLL5", "VDAC3", "CD1C", "AKR1C3", "PF4", "MYL9", "GNLY", "TREML1", "CA2")
-pbmc_tiny <- downsample_seurat(pbmc_tiny, features = gene_list)
-
+cell_list <- c("ATGCCAGAACGACT", "CATGGCCTGTGCAT", "GAACCTGATGAACC", "TGACTGGATTCTCA",
+			   "AGTCAGACTGCACA", "TCTGATACACGTGT", "TGGTATCTAAACAG", "GCAGCTCTGTTTCT",
+			   "GATATAACACGCAT", "AATGTTGACAGTCA", "AGGTCATGAGTGTC", "AGAGATGATCTCGC",
+			   "GGGTAACTCTAGTG", "CATGAGACACGGGA", "TACGCCACTCCGAA")
+pbmc_tiny <- ExpHunterSuite::downsample_seurat(pbmc_tiny, features = gene_list, cells = cell_list)
+counts <- pbmc_tiny$RNA@counts
+data <- pbmc_tiny$RNA@data
+scale.data <- pbmc_tiny$RNA@scale.data
+pbmc_tiny <- CreateSeuratObject(counts = counts, assay = "RNA", meta.data = pbmc_tiny@meta.data)
+pbmc_tiny$RNA$data <- data
+pbmc_tiny$RNA$scale.data <- scale.data
 usethis::use_data(pbmc_tiny, overwrite = TRUE)
