58bc2f32cb8fd7f0737827083453f8c4d9232cbf
James Richard Perkins <jimrperkins@gmail.com>
2026-01-23 12:43:00 +0100

    Fix n_category passing bug

--- R/write_report.R
+++ R/write_report.R
@@ -673,7 +673,7 @@ write_functional_report <- function(hunter_results, func_results, cores = 2,
     DEGH_results <- func_results$DEGH_results_annot
     enrichments_ORA_expanded <- func_results$WGCNA_ORA_expanded
     container <- list(hunter_results = hunter_results, max_genes = max_genes,
-                  func_results = func_results,
+                  func_results = func_results, n_category = showCategories,
                   model_organism = model_organism,
                   current_organism_info = current_organism_info,
                   flags_ora = flags_ora, flags_gsea = flags_gsea,
