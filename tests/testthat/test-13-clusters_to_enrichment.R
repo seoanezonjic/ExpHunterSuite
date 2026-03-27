b64f23b988e6bd97d76a77e6f1905ef43238aa02
James Richard Perkins <jimrperkins@gmail.com>
2026-03-27 13:53:38 +0100

    fixed broken test

--- tests/testthat/test-13-clusters_to_enrichment.R
+++ tests/testthat/test-13-clusters_to_enrichment.R
@@ -6,12 +6,16 @@ test_that("Run c2e main", {
   input_file <- system.file("extData", "cluster_genes.txt", package = "ExpHunterSuite")
   enr_lists <- main_clusters_to_enrichment(input_file, org_db=org_db, 
     current_organism_info=current_organism_info, gene_keytype="ENSEMBL")
+  top_cat <- as.data.frame(enr_lists$enrichments_ORA$MF$two)[1, "ID"]
+  
+  testthat::expect_equal(top_cat, "GO:0004866")
 
-   testthat::expect_equal(
-    as.data.frame(enr_lists$enrichments_ORA$MF$two)[1, "ID"], 
-    "GO:0004866")
-   compC <- clusterProfiler::merge_result(enr_lists$enrichments_ORA$MF)
-   showCats <- calc_showCat_compareCluster(compC, 10, 20)
-   testthat::expect_equal(showCats, 6)
+  
+  #compC <- clusterProfiler::merge_result(enr_lists$enrichments_ORA$MF)
+  compC <- process_cp_list(enr_lists$enrichments_ORA,
+    simplify_results = FALSE, clean_parentals = FALSE)
+  showCats <- calc_showCat_compareCluster(compC$MF@compareClusterResult,
+   10, 20)
+  testthat::expect_equal(showCats, 10)
 })
 
