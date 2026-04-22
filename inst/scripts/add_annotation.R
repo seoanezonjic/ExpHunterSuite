81fc85d2f4f20434ce22bcdd92e5adc82d177ab2
Álvaro Esteban Martos <alvaroesteban@uma.es>
2026-04-22 10:51:10 +0200

    Warning about untranslated IDs now properly mentions input keytype instead of assuming it's ensembl

--- inst/scripts/add_annotation.R
+++ inst/scripts/add_annotation.R
@@ -75,7 +75,7 @@ if (opt$mirna) {
   table_to_annot[,opt$output_keytype] <- translated_ids
   if(any(is.na(table_to_annot$SYMBOL))) {
     NAs <- which(is.na(table_to_annot$SYMBOL))
-    warning(paste0(length(NAs), " ensembl_IDs could not be translated."))
+    warning(paste(length(NAs), opt$input_keytype, "IDs could not be translated."))
     table_to_annot$SYMBOL[NAs] <- ids_to_translate[NAs]
   }
 }
