546cedc772bd6b212043d45ca4f98c5a1a323fe3
Álvaro Esteban Martos <alvaroesteban@uma.es>
2026-07-10 10:54:11 +0200

    get_kegg now only initialises ExpHunterSuite if it is not already initialsied. This avoids the script always trying to access main installation path even if it is being run in devel mode

--- inst/scripts/get_kegg.sh
+++ inst/scripts/get_kegg.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
-. ~soft_bio_267/initializes/init_degenes_hunter
+
+if [[ "$DEGHUNTER_MODE" == "" ]]; then
+	. ~soft_bio_267/initializes/init_degenes_hunter
+fi
+
 download_KEGG_file.R -O Human
 download_KEGG_file.R -O Mouse
